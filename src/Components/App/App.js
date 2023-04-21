d1daa3cad2bacd0b609d091cb0de5c8bb96177a4
karimy <karim.alrashdan14@gmail.com>
2023-04-21 03:03:04 -0600

    Complete useEffect in App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,20 +3,27 @@ import { Route } from "react-router-dom"
 import NavBar from '../NavBar/NavBar';
 import ArticleContainer from '../ArticleContainer/ArticleContainer';
 import { useState, useEffect } from "react"
+import fetchData from '../../apiCalls';
 
 function App() {
   const [homeArticles, setHomeArticles] = useState([])
 
-  useEffect =() => {
-    
-  }
+  useEffect(() => {
+    fetchData()
+    .then((data) => {
+      setHomeArticles(data.results)
+      // console.log("data", data.results)
+    })
+    .catch((error) => console.log(error))
+  }, [])
+
   return (
     <div className="App">
       <NavBar />
       <Route
         exact path="/"
         render={() =>
-          <ArticleContainer /> 
+          <ArticleContainer homeArticles={homeArticles}/> 
         }
       />
     </div>
