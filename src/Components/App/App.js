fe40561019d5c87f861ee9fe87b9090c7b23959e
karimy <karim.alrashdan14@gmail.com>
2023-04-21 01:30:36 -0600

    Add logo to NavBar

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
 import { Route } from "react-router-dom"
+import NavBar from '../NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
     </div>
   );
 }
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,7 +1,11 @@
 import "../NavBar/NavBar.css"
+import logo from "../../Images/news-logo.png"
 
 function NavBar() {
-
+  return (
+    
+    <img src={logo} alt="logo"></img>
+  )
 }
 
 export default NavBar;
