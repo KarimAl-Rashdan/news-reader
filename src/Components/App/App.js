9a4e81d5425a778f4ef92044f3655a52deca1acc
karimy <karim.alrashdan14@gmail.com>
2023-04-21 02:49:29 -0600

    Create fetchData function in apiCalls

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,8 +2,14 @@ import './App.css';
 import { Route } from "react-router-dom"
 import NavBar from '../NavBar/NavBar';
 import ArticleContainer from '../ArticleContainer/ArticleContainer';
+import { useState, useEffect } from "react"
 
 function App() {
+  const [homeArticles, setHomeArticles] = useState([])
+
+  useEffect =() => {
+    
+  }
   return (
     <div className="App">
       <NavBar />
