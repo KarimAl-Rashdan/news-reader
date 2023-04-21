4cdde161999d6dd6162fcd7470dc71086bab2704
karimy <karim.alrashdan14@gmail.com>
2023-04-21 03:13:07 -0600

    Pass down props to ArticleCard component

--- src/Components/ArticleContainer/ArticleContainer.js
+++ src/Components/ArticleContainer/ArticleContainer.js
@@ -1,8 +1,17 @@
 import "../ArticleContainer/ArticleContainer.css"
+import React from "react"
 
-function ArticleContainer() {
+function ArticleContainer({ homeArticles}) {
+  console.log("home Article", homeArticles)
   return (
-    <h1>Welcome to News Reader!</h1>
+    <div>
+      <h1>Welcome to News Reader!</h1>
+      <section className="card-grid">
+        {homeArticles.map((articleDetails) => {
+          return <ArticleCard image={articleDetails.multimedia[2].url} title={articleDetails.title} category={articleDetails.section} />
+        })}
+      </section>
+    </div>
   )
 }
 
