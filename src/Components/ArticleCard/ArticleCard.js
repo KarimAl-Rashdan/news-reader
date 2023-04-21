c18c64ae749b534730343ec5e21544548ff65575
karimy <karim.alrashdan14@gmail.com>
2023-04-21 03:53:06 -0600

    Complete initial styling for article cards

--- src/Components/ArticleCard/ArticleCard.js
+++ src/Components/ArticleCard/ArticleCard.js
@@ -8,8 +8,8 @@ function ArticleCard({id, image, title, category}) {
       <Link to={`/${id}`}>
         <img src={image} alt="article thumbnail" className="cardImage"/>
         <div className="cardInfo">
-          <p>{title}</p>
-          <p>{category}</p>
+          <p className="cardTitle">{title}</p>
+          <p className="cardCategory">Category: {category}</p>
         </div>
       </Link> 
     </button>
