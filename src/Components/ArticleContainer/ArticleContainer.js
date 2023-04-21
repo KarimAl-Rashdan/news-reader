cf56b36ac05a01e047e7871a2bc54fb669e6f21c
karimy <karim.alrashdan14@gmail.com>
2023-04-21 07:13:51 -0600

    Add filter functionality to home screen

--- src/Components/ArticleContainer/ArticleContainer.js
+++ src/Components/ArticleContainer/ArticleContainer.js
@@ -2,15 +2,20 @@ import "../ArticleContainer/ArticleContainer.css"
 import React from "react"
 import ArticleCard from "../ArticleCard/ArticleCard"
 
-function ArticleContainer({ homeArticles}) {
+function ArticleContainer({ filteredArticles, homeArticles}) {
   console.log("home Article", homeArticles)
+  console.log("filtered articles", filteredArticles)
   return (
     <div>
       <h1>Welcome to News Reader!</h1>
       <section className="card-grid">
-        {homeArticles.map((articleDetails, index) => {
+        {
+        filteredArticles.length>0 ? filteredArticles.map((articleDetails, index) => {
           return <ArticleCard id={index} key={index} image={articleDetails.multimedia[2].url} title={articleDetails.title} category={articleDetails.section} />
-        })}
+        }) : homeArticles.map((articleDetails, index) => {
+          return <ArticleCard id={index} key={index} image={articleDetails.multimedia[2].url} title={articleDetails.title} category={articleDetails.section} />
+        })  
+        }
       </section>
     </div>
   )
